6584b75db00a9c3750189c4e7e347dfc6c0e3358
david_5i3asud <david_5i3asud@DESKTOP-RVQ0VCF>
2017-10-15 17:56:09 +0200

    test bez static

---
+++ src/projektai/ProjektAI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projektai;
+
+/**
+ *
+ * @author david_5i3asud
+ */
+public class ProjektAI {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
